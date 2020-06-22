d193665ffee7f4b84da29dfe83f6dcc3ad882502
Kostyan1996 <meshavkin1996@gmail.com>
2020-06-22 03:43:39 +0300

    Added pages and basic routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { useRoutes } from './routes';
 
 function App() {
+  const routes = useRoutes(false);
   return (
-    <div>
-      Test
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        {routes}
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/routes.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { LinksPage } from './pages/links';
+import { ShortifyPage } from './pages/shortify';
+import { AuthPage } from './pages/auth';
+
+export const useRoutes = (isAuthenticated: boolean) => {
+  if (isAuthenticated) {
+    return (
+      <Switch>
+        <Route path="/links" exact>
+          <LinksPage />
+        </Route>
+        <Route path="/shortify" exact>
+          <ShortifyPage />
+        </Route>
+        <Redirect to="/shortify" />
+      </Switch>
+    );
+  }
+
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <AuthPage />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  );
+};
