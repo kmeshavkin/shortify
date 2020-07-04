2a9948e664df7ba5283a78232f23a3ec7de847d4
Kostyan1996 <meshavkin1996@gmail.com>
2020-07-04 15:06:42 +0300

    Add frontend links page

--- client/src/pages/links.tsx
+++ client/src/pages/links.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, {
+  useState, useCallback, useContext, useEffect,
+} from 'react';
+import { Spinner } from '@blueprintjs/core';
+import { useFetch } from '../hooks/fetch';
+import { AuthContext } from '../context/AuthContext';
+import { ILink } from '../../../models/Link';
 
-export const LinksPage = () => (
-  <div>
-    Links
-  </div>
-);
+export const LinksPage = () => {
+  const [links, setLinks] = useState<ILink[]>([]);
+  const { doFetch, loading } = useFetch();
+  const { token } = useContext(AuthContext);
+
+  const fetchLinks = useCallback(async () => {
+    const fetched = await doFetch('/api/link', 'GET', null, {
+      authorization: `Bearer ${token}`,
+    });
+    setLinks(fetched);
+  }, [token, doFetch]);
+
+  useEffect(() => {
+    fetchLinks();
+  }, [fetchLinks]);
+
+  if (loading) return <Spinner />;
+
+  if (!links || !links.length) return <p>No links yet</p>;
+
+  return (
+    <div>
+      {links.map((link) => (
+        <div key={link._id}>
+          <p>
+            From:
+            {' '}
+            {link.from}
+          </p>
+          <p>
+            To:
+            {' '}
+            {link.to}
+          </p>
+          <p>
+            Clicks:
+            {' '}
+            {link.clicks}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
