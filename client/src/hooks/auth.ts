7a3744942547976786d872e15d6f5c75e23ea92d
Kostyan1996 <meshavkin1996@gmail.com>
2020-08-01 08:14:51 +0300

    Fix cookie expiration bug and document that cookie doesn't work for session currently

--- client/src/hooks/auth.ts
+++ client/src/hooks/auth.ts
@@ -2,6 +2,7 @@ import { useState, useCallback } from 'react';
 import { IAuthContext } from '../context/AuthContext';
 
 export const useAuth = (): IAuthContext => {
+  // TODO: currently it doesn't work like session because it's always false after page refresh
   const [isLogged, setIsLogged] = useState<boolean>(false);
 
   const login = useCallback(() => {
--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -4,8 +4,9 @@ export const AuthMiddleware = (req, res, next) => {
   }
 
   try {
-    if (req.session.userId) return next();
+    if (req.session.userId && req.session.cookie.expires > new Date(Date.now())) return next();
   } catch (e) {
+    // TODO: also should destroy cookie if cookie expired so React can check "no cookie = redirect to /"
     return res.status(401).json({ message: 'Not authorized' });
   }
   return res.status(401).json({ message: 'Not authorized' });
