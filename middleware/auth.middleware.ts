f36030d075ca8c4dd579785b6de50fa501a62e20
Kostyan1996 <meshavkin1996@gmail.com>
2020-08-02 00:39:29 +0300

    Destroy cookie and session if user is not logged in or cookie expired

--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -1,3 +1,5 @@
+import config from 'config';
+
 export const AuthMiddleware = (req, res, next) => {
   if (req.method === 'OPTIONS') {
     return next();
@@ -5,9 +7,9 @@ export const AuthMiddleware = (req, res, next) => {
 
   try {
     if (req.session.userId && req.session.cookie.expires > new Date(Date.now())) return next();
-  } catch (e) {
-    // TODO: also should destroy cookie if cookie expired so React can check "no cookie = redirect to /"
-    return res.status(401).json({ message: 'Not authorized' });
-  }
-  return res.status(401).json({ message: 'Not authorized' });
+  } catch (e) { }
+  return req.session.destroy(() => {
+    res.clearCookie(config.get('sessionName'));
+    return res.status(401).json({ message: 'Not authorized' }); // TODO: why is .json required? status(200) doesn't work
+  });
 };
