b39b37bdf80ad774be2afdc5c4bda54305227540
Kostyan1996 <meshavkin1996@gmail.com>
2020-08-02 05:41:57 +0300

    Remove expiration cookie handling and cookie clearing

--- api/logout.ts
+++ api/logout.ts
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import config from 'config';
 
 const router = Router();
 router.post('/logout', async (req, res) => {
   try {
-    return req.session.destroy(() => {
-      res.clearCookie(config.get('sessionName'));
-      return res.json({ done: true }); // TODO: why is .json required? status(200) doesn't work
-    });
+    return req.session.destroy(() => res.json({ done: true })); // TODO: Check if I need to delete cookie myself
   } catch (error) {
     return res.status(500).json({ message: 'Something went wrong' });
   }
--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -1,15 +1,12 @@
-import config from 'config';
-
 export const AuthMiddleware = (req, res, next) => {
   if (req.method === 'OPTIONS') {
     return next();
   }
 
   try {
-    if (req.session.userId && req.session.cookie.expires > new Date(Date.now())) return next();
-  } catch (e) { }
-  return req.session.destroy(() => {
-    res.clearCookie(config.get('sessionName'));
-    return res.status(401).json({ message: 'Not authorized' }); // TODO: why is .json required? status(200) doesn't work
-  });
+    if (req.session.userId) return next();
+  } catch (e) {
+    return res.status(401).json({ message: 'Not authorized' });
+  }
+  return res.status(401).json({ message: 'Not authorized' });
 };
